52436435d659a0cd14b10b45824628d0a03c2a6b
Naman Jain <71509407+er-naman@users.noreply.github.com>
2024-09-23 18:54:31 +0000

    Variables

---
+++ Basics/01_Variables.js
@@ -0,0 +1,10 @@
+const accountID = 1208
+let accountEmail = "er.namanjn@gmail.com"
+var accountPass = "120812"
+accountCity = "Jaipur"
+let accountState;
+
+// in var there is an issue with block Scope and function scope
+
+console.log(accountID)
+console.table([accountID,accountEmail,accountPass,accountCity,accountState])
