0ea9fd88d4014f623ef16eadcce1e8418c2592a4
Naman Jain <71509407+er-naman@users.noreply.github.com>
2024-09-23 18:53:42 +0000

    Type of Datatypes

---
+++ Basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as new standard or newer version
+
+//alert(3 + 3); because we are not using browser
+
+let name = "Naman"
+let age = 26
+let isLogged = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined);  // undefined
+
+console.log(typeof null); // object
+
+
+
+
