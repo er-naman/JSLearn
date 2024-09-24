5dd022fbae6ec6011b3742f14ed35a1a993c1726
Naman Jain <71509407+er-naman@users.noreply.github.com>
2024-09-24 08:40:07 +0000

    Conversion operation

---
+++ Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "43"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+// "44" => 44
+// "44abc" => NaN
+// true => 1
+
+let loggedIn = 1
+
+let booleanIsLoggedIn = Boolean(loggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true
+// "" => false
+// "Naman" => true
+
+let num = 24
+let stringNum = String(num)
+console.log(stringNum);
+console.log(typeof stringNum);
