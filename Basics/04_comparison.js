e713dc560d59f345acc4446bcc1c27383b6cf06d
Naman Jain <71509407+er-naman@users.noreply.github.com>
2024-09-26 10:53:31 +0000

    Comparison

---
+++ Basics/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 => 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+
+// === strict check and this checks data types too
+
+console.log("2" === 2);
+console.log(2 === 2);
