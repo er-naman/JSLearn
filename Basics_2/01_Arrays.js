ca8c84ab0364d4db567fc9caac41760ba3bb83ae
Naman Jain <71509407+er-naman@users.noreply.github.com>
2024-10-29 20:04:15 +0000

    Arrays Again

---
+++ Basics_2/01_Arrays.js
@@ -0,0 +1,27 @@
+//----------------------------- Arrays-----------
+
+const myArray = [1,2,3,4,5]
+const Heros = ["Shaktiman", "BatMan", "Naman"]
+console.log(myArray);
+console.log(Heros);
+
+myArray.pop()
+Heros.pop()
+console.log(myArray);
+myArray.push(6)
+console.log(myArray);
+
+// myArray.unshift(12)
+myArray.shift()
+console.log(myArray);
+
+console.log(myArray.includes(3))
+console.log(myArray.indexOf(3))
+
+const newArray = myArray.join() // makes array in Strings
+
+// slice and Splice
+
+
+
+
