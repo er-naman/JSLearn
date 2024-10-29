944e261e3261bc1d6f47a8372e81d66dfe2a69ad
Naman Jain <71509407+er-naman@users.noreply.github.com>
2024-10-29 19:33:59 +0000

    Date and Time

---
+++ Basics/07_DatesAndTime.js
@@ -0,0 +1,17 @@
+// -------------- Dates---------------------
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.toString())
+console.log(newDate.toTimeString())
+console.log(newDate.toLocaleDateString())
+console.log(newDate.toDateString())
+console.log(newDate.toLocaleString())
+console.log(newDate.toUTCString())
+
+let myNewDate = new Date("12/08/1997")
+console.log(myNewDate.toLocaleString());
+
+
+let timeStamp = Date.now()
+console.log(timeStamp);
